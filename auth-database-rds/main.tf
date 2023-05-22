f58f7f86115f2aa251dcae0569228c1911cd0c41
Adam Howell <adam.howell@tombola.com>
2023-05-22 16:25:56 +0100

    trying fix for  connection issue

--- auth-database-rds/main.tf
+++ auth-database-rds/main.tf
@@ -2,12 +2,12 @@
 #defining the provider as aws
 provider "aws" {
     region     = "${var.region}"
-    profile = "${var.profile}"
+    proflie    = "${var.profile}"
 }
 
 resource "aws_db_subnet_group" "player-auth-db-subnet" {
   name       = "player-auth-db-subnet"
-  subnet_ids = ["subnet-02ae4f4268107c2b1", "subnet-012a534213cc034ec"]
+  subnet_ids = ["subnet-0379cfc4648718bc3", "subnet-02ae4f4268107c2b1", "subnet-012a534213cc034ec"]
   
   tags = {
     team = "Platform"
@@ -15,16 +15,17 @@ resource "aws_db_subnet_group" "player-auth-db-subnet" {
   }
 }
 
-resource "aws_db_instance" "player-auth-db" {
+resource "aws_db_instance" "default" {
   db_name = "playerauthdb"
-  identifier = "authdb"
+  identifier = "platform-player-auth-rds"
+  publicly_accessible = true
   allocated_storage = 20
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "password100"
+  username             = "root"
+  password             = "password"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  db_subnet_group_name = "player-auth-db-subnet"
+  db_subnet_group_name = "platform-player-auth"
 }
--- auth-database-rds/outputs.tf
+++ auth-database-rds/outputs.tf
@@ -1,1 +1,5 @@
 #outputs.tf
+output "player_auth_db_instance" {
+    value = aws_db_instance.default.db_name
+    depends_on = [ aws_db_subnet_group.player-auth-db-subnet ]
+}
